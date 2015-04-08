519687afd18a4eda3eba75fdd51a7553c3d6fa19
nchodelski <nchodelski@gmail.com>
2015-04-08 13:10:29 -0700

    updated assigment file, pics,  knitted docs.

--- code to knitr2html project file.R
+++ code to knitr2html project file.R
@@ -1,5 +1,9 @@
-library(knitr)
+# knits html
 getwd()
 setwd("/Users/Natalia/Coding/Reproducible_Research/RepData_PeerAssessment1/")
 
+library(knitr)
+library(markdown)
+
 knit2html("PA1_template.Rmd") 
+
